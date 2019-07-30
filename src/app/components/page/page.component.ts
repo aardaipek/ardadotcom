1e7208c53f577d005618b67844b377fd52ab3d41
Arda Ipek <aardaipek@outlook.com>
2019-07-30 14:08:26 +0300

    server

---
+++ src/app/components/page/page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.scss']
+})
+export class PageComponent implements OnInit {
+
+  today =new Date().getFullYear()
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+ 
+
+}
