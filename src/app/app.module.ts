6fa309db1450d665205e5f0aa20988c59497af4f
Arda İpek <arda.ipek@primeapps.io>
2020-08-21 11:47:05 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { PageComponent } from './components/page/page.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { WorksComponent } from './components/works/works.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageComponent,
     NavComponent,
-    HeroComponent
+    HeroComponent,
+    WorksComponent,
+    ProjectsComponent,
+    ContactComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/calculations.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculationsService {
+
+  constructor() { }
+
+
+  calculateExperiences(obj:Object) {
+    
+  }
+
+}
